50623e382f3adf3653d23f62b503bec9a7ae44ee
Jacky <jackymlui@live.com>
2017-08-10 08:54:22 +1000

    back to previous

--- src/components/RecipesList.js
+++ src/components/RecipesList.js
@@ -11,17 +11,17 @@ class RecipesList extends Component {
             if (localStorage['recipeStorage' + ii] === undefined) localStorage.removeItem('recipeStorage' + ii);
             else {
                 recipeLocalLine = JSON.parse(localStorage['recipeStorage' + ii]);
-                var recipeObject = { stockName: recipeLocalLine.stockName, quantity: recipeLocalLine.quantity, inStock: recipeLocalLine.inStock, localKey: recipeLocalLine.localKey};
+                var recipeObject = { stockName: recipeLocalLine.stockName, quantity: recipeLocalLine.quantity, inStock: recipeLocalLine.inStock, localKey: recipeLocalLine.localKey };
                 if (nameDirectory[recipeLocalLine.recipeName] === undefined) { // new recipe read in
                     nameDirectory[recipeLocalLine.recipeName] = readLocalRecipes.length;
                     readLocalRecipes.push([recipeLocalLine.recipeName, recipeLocalLine.showIngredients, [recipeObject]]);
-                } else{
+                } else {
                     readLocalRecipes[nameDirectory[recipeLocalLine.recipeName]][2].push(recipeObject);
                 }
             }
         }
-        
-        if (readLocalRecipes.length === 0) readLocalRecipes.push(['', false, [{ stockName: '', quantity: '', inStock: 'none', localKey:''}]]);
+
+        if (readLocalRecipes.length === 0) readLocalRecipes.push(['', false, [{ stockName: '', quantity: '', inStock: 'none', localKey: '' }]]);
         this.state = {
             recipesStorage: readLocalRecipes,
             editing: Array(readLocalRecipes.length).fill(false)
@@ -41,11 +41,11 @@ class RecipesList extends Component {
                         recipesStorage={this.state.recipesStorage}
                         changeRecipe={this.changeRecipe}
                         clickedRecipe={this.clickedRecipe.bind(this, ii)}
-                        clickedEdit={this.clickedEdit.bind(this,ii)}
+                        clickedEdit={this.clickedEdit.bind(this, ii)}
                         clickedAddIngred={this.clickedAddIngred.bind(this, ii)}
                         makeTonight={() => this.makeTonight}
                         editing={this.state.editing}
-                        deleteRow = {this.deleteRow}
+                        deleteRow={this.deleteRow}
                     />
                 </div>
             );
@@ -53,15 +53,15 @@ class RecipesList extends Component {
         return rows;
     }
 
-    deleteRow(ii,jj){ // done badly
+    deleteRow(ii, jj) { // done badly
         var contentObject = JSON.parse(JSON.stringify(this.state.recipesStorage));
         var localKey = contentObject[ii][2][jj].localKey;
-        contentObject[ii][2].splice(jj,1);
+        contentObject[ii][2].splice(jj, 1);
         localStorage.removeItem("recipeStorage" + localKey);
         this.setState({
             recipesStorage: contentObject
         })
-    }    
+    }
     makeTonight() {
         console.log("please fill this in!");
     }
@@ -69,7 +69,7 @@ class RecipesList extends Component {
     clickedEdit(ii) {
         var toggleEditing = JSON.parse(JSON.stringify(this.state.editing));
         console.log(ii);
-        toggleEditing[ii] = toggleEditing[ii]?false:true;
+        toggleEditing[ii] = toggleEditing[ii] ? false : true;
         console.log(toggleEditing);
         this.setState({
             editing: toggleEditing
@@ -80,12 +80,12 @@ class RecipesList extends Component {
         var contentObject = JSON.parse(JSON.stringify(this.state.recipesStorage));
         contentObject[ii][1] = (contentObject[ii][1]) ? false : true; // show ingredients
         // check that last ingredient had a title otherwise remove
-        if (contentObject[ii][1] === false){
-            for (var jj =0; jj < contentObject[ii][2].length; jj++){
-                if (contentObject[ii][2][jj]['stockName'].length === 0) contentObject[ii][2].splice(jj,1);
+        if (contentObject[ii][1] === false) {
+            for (var jj = 0; jj < contentObject[ii][2].length; jj++) {
+                if (contentObject[ii][2][jj]['stockName'].length === 0) contentObject[ii][2].splice(jj, 1);
             }
-            if (contentObject[ii][0].length === 0) contentObject.splice(ii,1);
-        }    
+            if (contentObject[ii][0].length === 0) contentObject.splice(ii, 1);
+        }
         this.setState({
             recipesStorage: contentObject
         })
@@ -109,17 +109,17 @@ class RecipesList extends Component {
                             break;
                         }
                     }
-                } else{
+                } else {
                     localStorage.removeItem("recipeStorage" + contentObject[row][2][ingredient]['localKey']);
                 }
                 var localKey = contentObject[row][2][ingredient]['localKey'];
                 console.log("localKey" + localKey);
                 // build localStorageRowObject
-                var localStorageObject = { recipeName: contentObject[row][0], showIngredients: contentObject[row][1]};
+                var localStorageObject = { recipeName: contentObject[row][0], showIngredients: contentObject[row][1] };
                 Object.assign(localStorageObject, contentObject[row][2][ingredient]);
                 // console.log(localStorageObject);
                 // Code for localStorage/sessionStorage.
-                
+
                 localStorage.setItem("recipeStorage" + localKey, JSON.stringify(localStorageObject));
                 console.log(localStorage["recipeStorage" + localKey]);
             } else {
@@ -163,12 +163,12 @@ class RecipesList extends Component {
             alert("Your recipe is not named!");
             return;
         }
-        contentObject.push(['', false, [{ stockName: '', quantity: '', inStock: 'none', localKey: ''}]]);
+        contentObject.push(['', false, [{ stockName: '', quantity: '', inStock: 'none', localKey: '' }]]);
         console.log(contentObject);
-        
+
         this.setState({
             recipesStorage: contentObject,
-            editing:pushNewEditing
+            editing: pushNewEditing
         })
     }
 
